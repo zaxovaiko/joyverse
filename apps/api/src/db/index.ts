8be71f96f23261cd6fc79d9f1c7f22aac31b76f2
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2025-08-07 17:53:48 +0200

    fix: remove pg and replace with bun-sql

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,7 +1,5 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/bun-sql";
 import { usersTable } from "./schema";
 
 const connectionString = process.env.DATABASE_URL!;
-const pool = new Pool({ connectionString });
-export const db = drizzle(pool, { schema: { usersTable } });
+export const db = drizzle(connectionString, { schema: { usersTable } });
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,23 @@
-import { Elysia } from "elysia";
 import "dotenv/config";
+
+import { Elysia } from "elysia";
 import { db } from "./db";
+import swagger from "@elysiajs/swagger";
+import cors from "@elysiajs/cors";
+import { rateLimit } from "elysia-rate-limit";
+import { sql } from "drizzle-orm";
 
 const app = new Elysia()
+  .use(cors())
+  .use(rateLimit())
+  .use(swagger())
   .get("/", async () => {
-    return !!(await db.execute("SELECT 1;")).rowCount;
+    return !!(await db.execute(sql`SELECT 1;`));
   })
-  .listen(3000);
+  .listen(4000);
 
 console.log(
-  `🦊 Server is running at ${app.server?.hostname}:${app.server?.port}`,
+  `🦊 Server is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
 
 export type App = typeof app;
